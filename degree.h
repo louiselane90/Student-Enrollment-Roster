7d7e116e22ca2b09bda2a2d35d3d123a5140a1f0
Wendy V <96085791+louiselane90@users.noreply.github.com>
2023-12-07 23:19:50 -0600

    Create degree.h

---
+++ degree.h
@@ -0,0 +1,5 @@
+#pragma once
+#include <string>
+
+enum class DegreeProgram { SECURITY, NETWORK, SOFTWARE, NA }; //enum class for degree programs
+static const std::string degreeProgramStrings[] = { "SECURITY", "NETWORK", "SOFTWARE", "NA"}; //degree program string
